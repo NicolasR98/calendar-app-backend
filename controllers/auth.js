c14c3aa8c123a0eb46963e4e72fd7fb2fd338812
Nicolas Rios <nicolas.rios@icareweb.com>
2022-04-01 22:12:59 +0200

    Add basic interaction for creating a user in the db

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,38 @@
-const createUser = (req, res) => {
-    const { email, name, password } = req.body;
+// Models
+const User = require("../models/User");
 
-    res.status(201).json({
-        ok: true,
-        msg: 'register',
-        email,
-        name,
-        password,
-    });
+const createUser = async (req, res) => {
+    const { email } = req.body;
+
+    try {
+        // Check if user exists in the db
+        let user = await User.findOne({ email });
+
+        if (user) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'The requested email is already in use'
+            });
+        };
+
+        // If it does not exist, then save it into the db
+        user = new User(req.body);
+        await user.save();
+
+        res.status(201).json({
+            ok: true,
+            uid: user.id,
+            name: user.name,
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Auth error, please try again',
+        });
+    }
+
 };
 
 const loginUser = (req, res) => {
