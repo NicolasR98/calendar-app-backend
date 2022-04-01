f04d06e0bd974ea817064226ccf40b5d7c9bee47
Nicolas Rios <nicolas.rios@icareweb.com>
2022-04-01 22:38:08 +0200

    Add basic interaction for login a user

--- controllers/auth.js
+++ controllers/auth.js
@@ -44,15 +44,42 @@ const createUser = async (req, res) => {
 
 };
 
-const loginUser = (req, res) => {
+const loginUser = async (req, res) => {
     const { email, password } = req.body;
 
-    res.status(200).json({
-        ok: true,
-        msg: 'login',
-        email,
-        password
-    });
+    try {
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Incorrect email',
+            });
+        };
+
+        // Confirm the password
+        const validPassword = bcrypt.compareSync(password, user.password);
+
+        if (!validPassword) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'Incorrect password'
+            });
+        };
+
+        return res.json({
+            ok: true,
+            uid: user.id,
+            name: user.name
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Auth error, please try again',
+        });
+    }
 };
 
 const renewToken = (req, res) => {
